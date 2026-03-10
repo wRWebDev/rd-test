d483f952b61a46c0af5ccb606d99cf8473927fba
Will Robertson <admin@wrweb.dev>
2026-03-10 11:54:26 +0000

    Rename "welcome" view to "order-form"

---
+++ resources/views/order-form.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    Homepage
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('order-form');
 });
 
 Route::get('products', [ProductController::class, 'index'])->name('products');
