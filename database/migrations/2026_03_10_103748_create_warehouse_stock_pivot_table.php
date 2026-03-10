d8eba0f2918998d9f0e4218b186543bf2464a970
Will Robertson <admin@wrweb.dev>
2026-03-10 11:15:34 +0000

    Fix typo in table names
    ... I need to slow down...

--- database/migrations/2026_03_10_103748_create_warehouse_stock_pivot_table.php
+++ database/migrations/2026_03_10_103748_create_warehouse_stock_pivot_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('warehouse_stock_pivot');
+        Schema::dropIfExists('warehouse_stock');
     }
 };
