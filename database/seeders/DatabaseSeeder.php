e0ec340e25be60bc0e70b74ad8264688b67efc34
wRWebDev <admin@wrweb.dev>
2026-03-10 14:21:52 +0000

    Add a seeder, creating 10 products, split between 10 warehouses

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Product;
+use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        /** @var Collection<int, Product> $products */
+        $products = Product::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        /** @var Collection<int, Warehouse> $warehouses */
+        $warehouses = Warehouse::factory(10)->create();
+
+        foreach ($products as $product) {
+            foreach ($warehouses as $warehouse) {
+                $warehouse->products()->attach($product->uuid, [
+                    'quantity' => fake()->numberBetween(10, 50),
+                    'threshold' => fake()->numberBetween(1, 10),
+                ]);
+            }
+        }
     }
 }
