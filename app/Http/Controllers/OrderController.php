173ce04dd31c054590e6189600901630ec694bd5
Will Robertson <admin@wrweb.dev>
2026-03-10 12:55:56 +0000

    Add validation for live stock count

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Closure;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\View\View;
@@ -19,13 +20,29 @@ class OrderController extends Controller
 
     public function store(FormRequest $request): View
     {
+        /** @var ?Product $product */
+        $product = null;
+
         $request->validate([
             'product' => 'required|string|exists:products,uuid',
-            'quantity' => 'required|integer|min:1',
+            'quantity' => [
+                'required',
+                'integer',
+                'min:1',
+                function (string $attribute, mixed $value, Closure $fail) use ($request, &$product) {
+                    $product = Product::findOrFail($request->input('product'));
+                    if ((int) $value > $product->immediateDespatch()) {
+                        $fail('Not enough stock to satisfy the order');
+                    }
+                },
+            ],
         ]);
 
-        $product = Product::find($request->input('product'));
         $quantity = $request->integer('quantity');
+
+        if (is_null($product)) {
+            $product = Product::findOrFail($request->input('product'));
+        }
 
         $order = Order::create([
             'status' => OrderStatus::PLACED,
