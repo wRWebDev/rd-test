cd538dd0599f3af136411de54842e40a3a188046
Will Robertson <admin@wrweb.dev>
2026-03-10 12:00:43 +0000

    Build out products index view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
-    @dump($products)
+    <x-title>Products</x-title>
+
+    <x-table :headers="[
+        'title',
+        'price',
+        'allocated to orders',
+        'physical quantity',
+        'total threshold',
+        'immediate dispatch',
+        '',
+    ]">
+        @foreach ($products as $product)
+            <tr>
+                <td>{{ $product->title }}</td>
+                <td>{{ $product->price / 100 }}</td>
+                <td>{{ $product->allocatedToOrders() }}</td>
+                <td>{{ $product->physicalQuantity() }}</td>
+                <td>{{ $product->totalThreshold() }}</td>
+                <td>{{ $product->immediateDespatch() }}</td>
+                <td class="text-right">
+                    <a href="{{ route('product', $product) }}" class="hover:underline">
+                        View
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    </x-table>
 </x-layout>
